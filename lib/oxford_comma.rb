e3215e2c472c1e346cce48ffe819a51eae7f17f3
Will Lee <will.i.lee@outlook.com>
2018-06-07 16:27:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,15 @@
 require 'pry'
 
 def oxford_comma(array)
+  if array.length == 2
+    new_string = array.join(" and ")
+    return new_string
+  end
+
+  i = 0
+  
   new_string = array.join(",")
+  
   binding.pry
   return new_string
 end
